c0125291828064ef063aa503d529cbfbb298d601
G Nitin <nitin.naik@rayabharitech.com>
2022-07-02 17:00:53 +0530

    added routes for the page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-// import 'antd/dist/antd.css';
+import 'antd/dist/antd.css';
 import '../src/assets/scss/styles.scss';
 import VirtualCards from './layouts/VirtualCards';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AllVirtualCard from './pages/All';
+import BlockedVirtualCard from './pages/Blocked';
+import YourVirtualCard from './pages/Your';
+import PageNotFound from './pages/PageNotFound';
 
 const App = () => {
   return (
-    <div>
+    <Router>
       <VirtualCards />
-    </div>
+      <Routes>
+        <Route path="/" element={<AllVirtualCard />} />
+        <Route path="/blocked" element={<BlockedVirtualCard />} />
+        <Route path="/your" element={<YourVirtualCard />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </Router>
   );
 };
 
