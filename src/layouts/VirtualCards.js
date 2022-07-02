94c8ec28e5d9f1b567b2a508cb8e9a90a726f6b2
G Nitin <nitin.naik@rayabharitech.com>
2022-07-02 16:06:58 +0530

    Implemented Tabs

--- src/layouts/VirtualCards.js
+++ src/layouts/VirtualCards.js
@@ -1,19 +1,39 @@
 import { VideoCameraOutlined, PlusOutlined } from '@ant-design/icons';
+import { Tabs } from 'antd';
+import { useState } from 'react';
+const { TabPane } = Tabs;
 
 const VirtualCards = () => {
+  const [tabList] = useState([
+    { key: 1, text: 'Your', value: 'your' },
+    { key: 2, text: 'All', value: 'all' },
+    { key: 3, text: 'Blocked', value: 'blocked' },
+  ]);
+
+  const tabPane = tabList.map(tab => (
+    <TabPane tab={tab.text} key={tab.key}>
+      {tab.value}
+    </TabPane>
+  ));
+
   return (
     <div className="virtual-card-wrapper">
-      <div className="virtual-card-wrapper__heading">
-        <h1>Virtual Cards</h1>
-        <span className="virtual-card-wrapper__heading__learn-more">
-          <VideoCameraOutlined /> Learn more
-        </span>
+      <div className="virtual-card-wrapper__header">
+        <div className="virtual-card-wrapper__heading">
+          <h1>Virtual Cards</h1>
+          <span className="virtual-card-wrapper__heading__learn-more">
+            <VideoCameraOutlined /> Learn more
+          </span>
+        </div>
+        <div>
+          <span className="virtual-card-wrapper__add-card">
+            <PlusOutlined className="virtual-card-wrapper__plus-icon" /> Virtual card
+          </span>
+        </div>
       </div>
-      <div>
-        <span className="virtual-card-wrapper__add-card">
-          <PlusOutlined className="virtual-card-wrapper__plus-icon" /> Virtual card
-        </span>
-      </div>
+      <Tabs size="large" tabBarStyle={{ color: 'grey' }} tabBarGutter={40} defaultActiveKey="1">
+        {tabPane}
+      </Tabs>
     </div>
   );
 };
