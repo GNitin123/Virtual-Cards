082d22110e22fa713bb2537cda1ee3b2de329e3f
G Nitin <nitin.naik@rayabharitech.com>
2022-07-03 19:52:19 +0530

    handled listing of your cards

--- src/pages/Your/index.js
+++ src/pages/Your/index.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import { useContext } from 'react';
+import Card from '../../components/Helper/Card';
+import VirtualCardContext from '../../context/VirtualCardContext';
 
 const YourVirtualCard = () => {
-  return <div>YourVirtualCard</div>;
+  const { cardList } = useContext(VirtualCardContext);
+
+  const card = cardList.map((card, index) => <Card key={index} cardContent={card} />);
+
+  return (
+    <>
+      <div className="page-card-wrapper">{card}</div>
+    </>
+  );
 };
 
 export default YourVirtualCard;
