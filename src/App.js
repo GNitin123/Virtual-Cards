222358db0ee9d046045818275242a6e2cefa5d41
G Nitin <nitin.naik@rayabharitech.com>
2022-07-12 22:51:52 +0530

    css changed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import 'antd/dist/antd.css';
 import '../src/assets/scss/styles.scss';
+import 'antd/dist/antd.min.css';
 import VirtualCards from './layouts/VirtualCards';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AllVirtualCard from './pages/All';
